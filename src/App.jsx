330d2f460ee5f803d0bc19962a9971a0ebb981c7
develov3r <baltazargarciacanllo@gmail.com>
2023-01-24 16:44:46 -0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
-import { Route, Router, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./screen/Home";
 
 function App() {
   return (
     <Routes>
-      
+      <Route path="/" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
