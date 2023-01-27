e890bf14e329cc2e56c83034e7f9b6d130b2b9d9
nicolas <nicolastolini@hotmail.com>
2023-01-26 21:32:26 -0300

    login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./screen/Home";
+import Login from "./screen/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
