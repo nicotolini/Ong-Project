1b6338629eda24fe8e2c91e24ed9be30eb0f3ad1
nicolas <nicolastolini@hotmail.com>
2023-02-02 22:20:37 -0300

    fix icon

--- src/component/ButtonHome.jsx
+++ src/component/ButtonHome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const ButtonHome = () => {
   return (
     <div className="flex items-center">
-      <img src="/searchicon.svg" alt="search-icon" />
+      <img src="/searchicon.svg" alt="search-icon" className="w-20" />
 
       <h3 className="text-[32px] ml-[15px] text-white-100">Job Search</h3>
     </div>
