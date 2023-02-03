3a6d19e599649960ad584131180f2dbbc12f1395
Matias <matias.sanchez.0097@gmail.com>
2023-02-02 22:41:59 -0300

    adding changes

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+const candidates = [
+  { name: "Mei Nagano", job: "UI/UI Designer", image: "./src/assets/images-Candidates/one.png" },
+  { name: "Taise Marukawa", job: "Web Programmer", image: "./src/assets/images-Candidates/two.png" },
+  { name: "John Jerry", job: "Sales Markting", image: "./src/assets/images-Candidates/three.png" },
+  { name: "Hachiman", job: "UI Designer", image: "./src/assets/images-Candidates/four.png" },
+  { name: "Merry", job: "UX Designer", image: "./src/assets/images-Candidates/five.png" },
+  { name: "Kazuha", job: "Web Designer", image: "./src/assets/images-Candidates/six.png" },
+  { name: "Ei", job: "Back End Progammer", image: "./src/assets/images-Candidates/seven.png" },
+  { name: "Lucia", job: "Fornt End Programmer", image: "./src/assets/images-Candidates/eight.png" },
+  { name: "Luna", job: "UI/UX Designer", image: "./src/assets/images-Candidates/nine.png" },
+  { name: "Shin", job: "UI/UI Designer", image: "./src/assets/images-Candidates/ten.png" },
+  { name: "Raiden Shuga", job: "Script Writer", image: "./src/assets/images-Candidates/eleven.png" },
+  { name: "Anju Emma", job: "UI/UI Designer", image: "./src/assets/images-Candidates/twelve.png" },
+];
+
+export { candidates };
