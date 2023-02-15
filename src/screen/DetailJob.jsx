477e9f7925d719a0658c00d7e076e7b7dd933816
Matias <matias.sanchez.0097@gmail.com>
2023-02-15 11:45:20 -0300

    changes in detail job page

--- src/screen/DetailJob.jsx
+++ src/screen/DetailJob.jsx
@@ -5,19 +5,26 @@ import Form from "../component/Form/Form";
 import JobDescription from "../component/JobDescription";
 import Footer from "../component/Footer";
 import { jobs } from "../constants";
+import { useParams } from "react-router-dom";
 
 const DetailJob = () => {
+  const { id } = useParams();
+  const job = jobs.find((job) => job._id === id);
+
   return (
     <div className="bg-violet-100">
-      <Navbar />
       <SubHeading subTitle="" title="Titulo job" />
-      <JobDescription
-        country={jobs[9].country}
-        jobTitle={jobs[5].job}
-        image={jobs[4].image}
-        modality={jobs[5].modality}
-      />
-      <Form />
+
+      {job ? (
+        <>
+          <JobDescription country={job.country} jobTitle={job.job} image={job.image} modality={job.modality} />
+          <Form />
+        </>
+      ) : (
+        <div className="flex_center">
+          <h4>No se encontro el trabajo</h4>
+        </div>
+      )}
       <Footer />
     </div>
   );
